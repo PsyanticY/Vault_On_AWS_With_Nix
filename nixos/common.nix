a20242ebe7cdcd677dd537f7b758c61e01144d64
PsyanticY <iuns@outlook.fr>
2019-07-23 14:12:06 +0100

    Update motd and common config

--- nixos/common.nix
+++ nixos/common.nix
@@ -5,20 +5,39 @@
   environment.systemPackages = with pkgs; [ vim python3 jq awscli tree curl consul];
   environment.shellAliases = { tailf = "tail -f" ;};
 
+  environment.shellInit = ''
+    export EDITOR=vim
+    export HISTFILESIZE=50000
+    export HISTCONTROL=erasedups
+    shopt -s histappend
+    export PROMPT_COMMAND="history -a"
+    export HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S - '
+    export PYTHONDONTWRITEBYTECODE=1
+  '';
+  programs.bash.enableCompletion = true;
+  security.pam.enableSSHAgentAuth = true;
+  users.motd = ''
+      ===###===###===###===###===###===###===###===###===###===###===###===
+
+
+                  ____                  _       _  ___
+                 |  _ \  _____   ____ _| |__   | |/ (_)_ __
+                 | | | |/ _ \ \ / / _` | '_ \  | ' /| | '_ \
+                 | |_| | (_) \ V / (_| | | | | | . \| | | | |
+                 |____/ \___/ \_/ \__,_|_| |_| |_|\_\_|_| |_|
+
+                 Dovahkiin, Dovahkiin, naal ok zin los vahriin
+                     Wah dein vokul mahfaeraak ahst vaal!
+              Ahrk fin norok paal graan fod nust hon zindro zaan
+                     Dovahkiin, fah hin kogaan mu draal!
+
+      ===###===###===###===###===###===###===###===###===###===###===###===
+
+    '';
+
   deployment.keys = {
-    "dovah.crt".text = builtins.readFile <global_creds/predictix.crt>;
-    "dovah.key".text = builtins.readFile <global_creds/predictix.key>;
-    "root.ca".text = builtins.readFile <global_creds/digicert.ca>;
+    "dovah.crt".text = builtins.readFile <global_creds/dovah.crt>;
+    "dovah.key".text = builtins.readFile <global_creds/dovah.key>;
+    "root.ca".text = builtins.readFile <global_creds/root.ca>;
     };
-  # datadog monitoring
-  #services.datadog-agent.enable = true;
-  #services.datadog-agent.apiKeyFile = "/etc/keys/datadog_api_key";
-  #environment.etc = {
-  #  target = "keys/datadog_api_key";
-  #  source = <keys/datadog_api_key>;
-  #  mode = "0440";
-  #};
-  # add checks for common important systemd units (sssd, sshd)
-  # ...
-  # ...
 }
